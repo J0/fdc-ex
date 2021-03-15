0fda483f93e6887ee80b88dc5b0dc265c030e41c
Joel Lee <joel@Joels-MacBook-Pro-3.local>
2021-03-15 09:03:04 +0800

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -11,8 +11,7 @@ defmodule FdcEx.MixProject do
       package: package(),
       deps: deps(),
       name: "fdc-ex",
-      source_url: "https://github.com/j0/fdc-ex",
-      deps: deps()
+      source_url: "https://github.com/j0/fdc-ex"
     ]
   end
 
@@ -23,7 +22,7 @@ defmodule FdcEx.MixProject do
   defp package() do
     [
       # This option is only needed when you don't want to use the OTP application name
-      name: "postgrestex",
+      name: "fdc_ex",
       # These are the default files included in the package
       files: ~w(lib .formatter.exs mix.exs README*  LICENSE*
                  ),
@@ -43,7 +42,10 @@ defmodule FdcEx.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.7"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      # dev dependencies
+      {:ex_doc, "~> 0.13", only: :dev, runtime: false},
+      {:dialyxir, "~> 0.5", only: :dev, runtime: false}
 
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
