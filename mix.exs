b0896ec5a59fa7f4f47586802d52bfdaaecbbf8d
Joel Lee <joel@Joels-MacBook-Pro-3.local>
2021-03-15 08:58:35 +0800

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,28 @@ defmodule FdcEx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "fdc-ex",
+      source_url: "https://github.com/j0/fdc-ex",
       deps: deps()
+    ]
+  end
+
+  defp description() do
+    "This is a elixir client library for Food Data Central, built with the intention of supporting a Nutrition Calculator."
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "postgrestex",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README*  LICENSE*
+                 ),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/j0/fdc-ex"}
     ]
   end
 
